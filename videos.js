55e79e0a32448503de8df6ce56203fbe1f906b21
mraeroxol-boop <mraeroxol@gmail.com>
2025-08-14 17:48:10 +0600

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,12 @@
+// Only you edit this file to add new videos
+const videosData = [
+  {
+    title: "race master mod menu apk",
+    url: "https://youtu.be/2ZRUTe_iSwE?si=fRJpo7oMxRlJL53P"
+  },
+  {
+    title: "new gfx pack",
+    url: "https://youtu.be/onSPV5l3hqU?si=hekSO1f4jrwOEO8v"
+  },
+  // Add more videos here
+];
